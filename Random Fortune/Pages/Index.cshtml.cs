3464f702ff28fe529dfa40f2076d93c9c41fdaec
korxaq <jsoto@imail.ag>
2020-08-19 17:09:39 -0600

    Initial Project

---
+++ Random Fortune/Pages/Index.cshtml.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+using Random_Fortune.Helper;
+
+namespace Random_Fortune.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+
+        public string FortuneMessage { get; set; }
+
+        public IndexModel(ILogger<IndexModel> logger)
+        {
+            _logger = logger;
+        }
+
+        public void OnGet()
+        {
+            var helper = new FortuneHelper();
+
+            FortuneMessage = helper.Fortune();
+        }
+    }
+}
